26dac92a49758a80f3ec367f40bb5d7937aa2e38
lh <lhbulingbulingstar@gmail.com>
2024-09-06 15:53:56 +0800

    依赖优化

---
+++ src/main/java/com/lh/cloud/common/entity/User.java
@@ -0,0 +1,28 @@
+package com.lh.cloud.common.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Date: 2022/6/22 16:12
+ * @author lh
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("sys_user")
+public class User extends BaseEntity {
+    @TableId("id")
+    private String id;
+
+    @TableField("name")
+    private String name;
+
+    @TableField("account")
+    private String account;
+
+    @TableField("password")
+    private String password;
+}
